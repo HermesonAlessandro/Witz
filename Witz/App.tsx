16992c7e1d2c6bf411e272acf870c287a499a929
Hermeson Alessandro Melo de Sousa <105324312+HermesonAlessandro@users.noreply.github.com>
2026-06-12 22:05:01 -0300

    Alterações
    Alterações

--- Witz/App.tsx
+++ Witz/App.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { MetasProvider } from './src/pages/goals/MetasContext';
 import Routes from './src/routes';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <MetasProvider>
-        <NavigationContainer>
           <Routes />
-        </NavigationContainer>
-      </MetasProvider>
     </SafeAreaProvider>
   );
 }
--- Witz/src/routes/index.tsx
+++ Witz/src/routes/index.tsx
@@ -1,32 +1,24 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Goals from '../pages/goals';
-import CriarMetaScreen from '../pages/goals/indexCreateGoals';
-import Main_screen from '../pages/main_screen';
-import Initial from '../pages/initial';
+
+import Initial from '../pages/initial'; 
 import Login from '../pages/login';
 import Register from '../pages/register';
-import ResetPassword from '../pages/resetPassword';
+import Resetpassoword from '../pages/resetPassword';
+
 
 const Stack = createStackNavigator();
 
 export default function Routes() {
   return (
+    <NavigationContainer>
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="main_screen" component={Main_screen} />
-      <Stack.Screen name="goals" component={Goals} />
-      <Stack.Screen
-        name="indexCreateGoals"
-        component={CriarMetaScreen}
-        options={{
-          presentation: 'transparentModal',
-          animation: 'fade',
-        }}
-      />
-      <Stack.Screen name="Initial" component={Initial} />
-      <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="Register" component={Register} />
-      <Stack.Screen name="ResetPassword" component={ResetPassword} />
+    <Stack.Screen name="Initial" component={Initial} />
+    <Stack.Screen name="Login" component={Login} />
+    <Stack.Screen name="Register" component={Register} />
+    <Stack.Screen name="ResetPassword" component={Resetpassoword} />
     </Stack.Navigator>
+    </NavigationContainer>
   );
 }
